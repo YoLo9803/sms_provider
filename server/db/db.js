f688d2c22acfdba3069b0ab420e7ddb6b2a903c9
chris_fs_wang <f98989000@gmail.com>
2020-09-10 19:19:47 +0800

    更改db config

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,19 @@
+// module.exports = {
+//     mysql: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: '1234',
+//         port: '3306',
+//         database: 'sms_pva'
+//     }
+// }
+
 module.exports = {
     mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
+        host: 'us-cdbr-east-02.cleardb.com',
+        user: 'bb410514d97f93',
+        password: 'c435a9a2',
         port: '3306',
-        database: 'sms_pva'
+        database: 'heroku_438489a76ca89e7'
     }
 }
